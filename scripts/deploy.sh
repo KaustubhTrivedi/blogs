34cde74f6a9a58177d57cd17bfb4d16cd2291dbe
Kaustubh Trivedi <kaus12tri@gmail.com>
2025-07-11 15:02:51 +0100

    Update deployment script to reference the correct path for the production Docker Compose file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Configuration
-COMPOSE_FILE="docker-compose.prod.yml"
+COMPOSE_FILE="../docker-compose.prod.yml"
 SERVICE_NAME="blog"
 
 # Functions
