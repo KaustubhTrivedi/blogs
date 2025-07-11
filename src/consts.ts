bce4778336535de4a324bfd8ee12adf99ac2bacc
Kaustubh Trivedi <kaus12tri@gmail.com>
2025-07-11 14:57:47 +0100

    Add Tailwind CSS integration and update site details
    - Integrated Tailwind CSS into the Astro project by adding the necessary configuration and dependencies.
    - Updated the site title and description to reflect the new branding.
    - Refactored the Header component for a simplified design.
    - Enhanced the main page to dynamically display blog posts with improved styling.
    - Removed the old blog index page as its functionality is now incorporated into the main page.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Bloggr';
+export const SITE_DESCRIPTION = 'Welcome to Kaustubh Trivedi blogs - Explore a world of insightful articles, thought-provoking essays, and engaging stories.';
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,js,jsx,ts,tsx,mdx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
